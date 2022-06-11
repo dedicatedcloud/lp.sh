b1166d66f3182086fa3c09a1274c3ba6ee1c434b
dedicatedcloud <98156514+dedicatedcloud@users.noreply.github.com>
2022-06-11 02:33:19 +0100

    ma

--- lp.sh
+++ lp.sh
@@ -5,24 +5,24 @@ echo -e "Please, provide us with a user name: "
   read password
 P_IP="`wget http://ipinfo.io/ip -qO -`"
 echo $P_IP
-apt update && apt -y upgrade && apt -y install ufw
-ufw allow OpenSSH && echo "y" | ufw enable
-ufw allow 80
-ufw allow 443
-apt -y install apache2
-apt -y install software-properties-common
-apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
+sudo apt update && sudo apt -y upgrade && sudo apt -y install ufw
+sudo ufw allow OpenSSH && echo "y" |sudo ufw enable
+sudo ufw allow 80
+sudo ufw allow 443
+sudo apt -y install apache2
+sudo apt -y install software-properties-common
+sudo apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
 add-apt-repository 'deb [arch=amd64] http://mariadb.mirror.globo.tech/repo/10.5/ubuntu focal main'
-apt update && apt -y install mariadb-server mariadb-client
+sudo apt update && apt -y install mariadb-server mariadb-client
 
-apt -y install php libapache2-mod-php php-mysql php-curl php-gd php-json php-mbstring php-imap php-iconv php-dev
+sudo apt -y install php libapache2-mod-php php-mysql php-curl php-gd php-json php-mbstring php-imap php-iconv php-dev
 
-apt update
-apt -y install build-essential curl nano wget lftp unzip bzip2 arj nomarch lzop htop openssl gcc git binutils libmcrypt4 libpcre3-dev make python3 python3-pip supervisor unattended-upgrades whois zsh imagemagick uuid-runtime net-tools
+sudo apt update
+sudo apt -y install build-essential curl nano wget lftp unzip bzip2 arj nomarch lzop htop openssl gcc git binutils libmcrypt4 libpcre3-dev make python3 python3-pip supervisor unattended-upgrades whois zsh imagemagick uuid-runtime net-tools
 
-apt install certbot python3-certbot-apache -y
-mkdir /var/www/$P_IP
-chown -R $USER:$USER /var/www/$P_IP
+sudo apt install certbot python3-certbot-apache -y
+sudo mkdir /var/www/$P_IP
+sudo chown -R $USER:$USER /var/www/$P_IP
 
 cat >/etc/apache2/sites-available/$P_IP.conf <<EOL
 <VirtualHost *:80>
@@ -39,11 +39,11 @@ cat >/etc/apache2/sites-available/$P_IP.conf <<EOL
 </VirtualHost>
 EOL
 
-a2enmod rewrite
-a2ensite $P_IP
-a2dissite 000-default
-systemctl reload apache2
-systemctl restart apache2
+sudo a2enmod rewrite
+sudo a2ensite $P_IP
+sudo a2dissite 000-default
+sudo systemctl reload apache2
+sudo systemctl restart apache2
 cat >/var/www/$P_IP/index.html <<EOL
 
 <html>
@@ -57,7 +57,7 @@ cat >/var/www/$P_IP/index.html <<EOL
   </body>
 </html>
 EOL
-rm /etc/apache2/mods-enabled/dir.conf
+sudo rm /etc/apache2/mods-enabled/dir.conf
 cat /etc/apache2/mods-enabled/dir.conf <<EOL
 <IfModule mod_dir.c>
         DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm
@@ -65,43 +65,11 @@ cat /etc/apache2/mods-enabled/dir.conf <<EOL
 EOL
 
 
-mysql_secure_installation <<EOF
-y
-Newcloudpass22##
-y
-y
-y
-y
-EOF
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
-echo "alias wp='~/wp-cli.phar'" >> .bashrc
-apt update && apt autoremove -y
-apt clean
 
-wget https://raw.githubusercontent.com/dedicatedcloud/lp.sh/main/new.sh 
+sudo curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+sudo chmod +x wp-cli.phar
+sudo mv wp-cli.phar /usr/local/bin/wp
+sudo echo "alias wp='~/wp-cli.phar'" >> .bashrc
+sudo apt update && apt autoremove -y
+sudo apt clean
 
-chmod +x new.sh
-mv new.sh /usr/local/bin/new
-
-echo "alias new='~/usr/local/bin/new'" >> .bashrc
-
-adduser $user_name <<EOF
-$password
-$password
-#
-#
-#
-#
-y
-y
-EOF
-
-usermod -aG sudo $user_name
-
-su $user_name
-
-cd /
-
-sudo apt update
--- new.sh
+++ new.sh
@@ -12,25 +12,32 @@ echo -e "Please, provide us with your domain name: "
 db_name="wp`date +%s`"
 db_user=$db_name
 db_password=`date |md5sum |cut -c '1-12'`
+sleep 1
+mysqlrootpass=`date |md5sum |cut -c '1-12'`
+sleep 1
 
-sudo chown -R $USER:$USER /var/www/html
-cd /var/www/html
-wp core download --path=$domain_name
-wp core config --dbname=$db_name --dbuser=$db_name --dbpass=$db_password
-wp db create
-wp core install --url=https://$domain_name --title=$site_title --admin_user=$user_name --admin_email=$domain_email --admin_password=$password
+mysql_secure_installation <<EOF
+y
+$mysqlrootpass
+y
+y
+y
+y
+EOF
 
-
+cd ~/
+mkdir -p $domain_name/logs $domain_name/public
+chmod -R 755 $domain_name
 
 cat >/etc/apache2/sites-available/$domain_name.conf <<EOL
 <VirtualHost *:80>
     ServerName $domain_name
     ServerAlias www.$domain_name
     ServerAdmin $domain_email
-    DocumentRoot /var/www/html/$domain_name
-    ErrorLog ${APACHE_LOG_DIR}/error.log
-    CustomLog ${APACHE_LOG_DIR}/access.log combined
-    <Directory /var/www/html/$domain_name/>
+    DocumentRoot ~/$domain_name/public
+    ErrorLog ~/$domain_name/error.log
+    CustomLog ~/$domain_name/access.log combined
+    <Directory ~/$domain_name/public>
 	   AllowOverride All
     </Directory>
 </VirtualHost>
@@ -40,3 +47,17 @@ sudo a2ensite $domain_name
 sudo systemctl reload apache2
 sudo systemctl restart apache2
 
+mysql -u root -p <<EOL
+$mysqlrootpass
+CREATE DATABASE $db_name CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci;
+CREATE USER '$db_name'@'localhost' IDENTIFIED BY '$db_password';
+
+
+
+
+cd ~/$domain_name/public
+
+wp core download
+wp core config --dbname=$db_name --dbuser=$db_name --dbpass=$db_password
+wp db create
+wp core install --url=https://$domain_name --title=$site_title --admin_user=$user_name --admin_email=$domain_email --admin_password=$password
