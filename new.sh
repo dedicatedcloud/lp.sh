6f41be1867506f8666dccd0b84fa29d8df0de4cd
dedicatedcloud <98156514+dedicatedcloud@users.noreply.github.com>
2022-06-11 01:30:02 +0100

    html

--- new.sh
+++ new.sh
@@ -13,8 +13,8 @@ db_name="wp`date +%s`"
 db_user=$db_name
 db_password=`date |md5sum |cut -c '1-12'`
 
-sudo chown -R $USER:$USER /var/www/*
-wp core download --path=/var/www/$domain_name --locale=en_US
+sudo chown -R $USER:$USER /var/www/html
+wp core download --path=/var/www/html/$domain_name --locale=en_US
 wp config create --dbname=$db_name --dbuser=$db_name --dbpass=$db_password
 wp db create
 wp core install --url=$domain_name --title="$site_title" --admin_user=$user_name --admin_password=$user_password --admin_email=$domain_email
@@ -26,10 +26,10 @@ cat >/etc/apache2/sites-available/$domain_name.conf <<EOL
     ServerName $domain_name
     ServerAlias www.$domain_name
     ServerAdmin $domain_email
-    DocumentRoot /var/www/$domain_name
+    DocumentRoot /var/www/html/$domain_name
     ErrorLog ${APACHE_LOG_DIR}/error.log
     CustomLog ${APACHE_LOG_DIR}/access.log combined
-    <Directory /var/www/$domain_name/>
+    <Directory /var/www/html/$domain_name/>
 	   AllowOverride All
     </Directory>
 </VirtualHost>
